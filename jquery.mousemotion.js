1c185aa7b9710ac6a7d0308451253c3af53d2877
Morgan Brown <brown.mhg@gmail.com>
2012-10-17 22:31:46 -0700

    Add ability to throttle mouse motion callback

--- jquery.mousemotion.js
+++ jquery.mousemotion.js
@@ -13,40 +13,60 @@
 
       var mm = this;
 
+      /* The number of miliseconds that must pass between executions of the mousemotion handler */
+      this.THROTTLE_WAIT = 100;
+
       /**
        * Attach a mousemove listener to the set of matched elements, which receives both an
        * Event and Frame object.  The Frame object contains information about the mouse's motion.
        *
        * @param {object} eventData
        * @param {function(eventObject, frame)} handler The function to be called when the mousemove event is fired.
+       * @param {boolean} If the handler function should be called only after $.MouseMotion.THROTTLE_WAIT miliseconds have elapsed.
        * @return {array} The set of matched elements.
        **/
-      this.mousemotion = function( eventData, handler ) {
-        var lastFrame;
+      this.mousemotion = function( eventData, handler, shouldThrottle ) {
+        var lastFrame, proxyHandler;
 
         if( handler == null ) {
           handler = eventData;
           eventData = null;
         }
 
-        return this.mousemove( eventData, function( event ) {
-          var frame = new mm.Frame( event.pageX, event.pageY, +new Date(), lastFrame );
-          lastFrame = frame;
-          return handler.call( this, event, frame );
-        });
+        // default to throttling
+        if( typeof shouldThrottle === 'undefined' ) {
+          shouldThrottle = true;
+        }
+
+        proxyHandler = function( event ) {
+            var frame = new mm.Frame( event.pageX, event.pageY, +new Date(), lastFrame );
+            lastFrame = frame;
+            handler.call( this, event, frame );
+        };
+
+        return this.mousemove( eventData, shouldThrottle && mm.throttle(proxyHandler) || proxyHandler );
       };
 
       /**
-       * This atan2 function returns the arctangent of the quotient of its arguments,
-       * as a numeric value between 0 and 2PI radians.
+       * Ensure that a function fun is called no more than once per wait milliseconds.
        *
-       * @param {number} y A number representing the y coordinate
-       * @param {number} x A number representing the x coordinate
-       * @return {number} A number between 0 and 2PI, or NaN if the value(s) are empty
+       * @see http://ryhan.org/post/33374611308/limiting-function-calls
+       * @param {function} fun The function to be throttled
+       * @param {number} The number of miliseconds that must pass before the next execution
        **/
-      this.atan2 = function( y, x ) {
-        return Math.PI + Math.atan2( y, x );
-      };
+      this.throttle = function( fun, wait ) {
+          var lastCalledAt = 0;
+
+          return function() {
+             var now = +new Date(),
+               context = this;
+
+             if ( now > lastCalledAt + wait ) {
+                lastCalledAt = now;
+                return fun.apply( context, arguments );
+             }
+          }
+      }
 
       /**
        * A Frame represents a snapshot of mouse movement at a given time.
@@ -71,12 +91,25 @@
         this.dt = this.t - lastFrame.t;
         // displacement is the same as distance
         this.displacement = Math.sqrt( Math.pow( this.dx, 2 ) + Math.pow( this.dy, 2 ) );
-        // direction is given as the number of radians from the positive x axis
+        // direction is given as the number of radians from the positive x axis, whose origin
+        // lies on (x, y)
         this.direction = mm.atan2( this.dy, -this.dx );
         // speed is in pixels/ms
         this.speed = this.displacement / this.dt;
         // pixels/ms^2
         this.acceleration = ( this.speed - lastFrame.speed ) / this.dt;
+      };
+
+      /**
+       * This atan2 function returns the arctangent of the quotient of its arguments,
+       * as a numeric value between 0 and 2PI radians.
+       *
+       * @param {number} y A number representing the y coordinate
+       * @param {number} x A number representing the x coordinate
+       * @return {number} A number between 0 and 2PI, or NaN if the value(s) are empty
+       **/
+      this.atan2 = function( y, x ) {
+        return Math.PI + Math.atan2( y, x );
       };
   };
 
